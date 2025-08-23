e64a50b8daa947f6fb09a98db656f239006d66cc
seethalakshmivenkatesh <seethavenkatesh79@gmail.com>
2025-08-23 11:37:38 +0530

    comment

--- frontend (2)/frontend/src/Home/HomeParent.jsx
+++ frontend (2)/frontend/src/Home/HomeParent.jsx
@@ -1,15 +1,16 @@
-import { Home } from '@mui/icons-material'
-import React from 'react'
-import HomeProducts from './HomeProducts'
-
+import { Home } from "@mui/icons-material";
+import React from "react";
+import HomeProducts from "./HomeProducts";
 
 function HomeParent() {
   return (
     <div>
-      <h1 className="text-2xl ml-13 mt-4 font-semibold font-serif">Best Sellers-Shopping Time</h1>
-    <HomeProducts />
+      <h1 className="text-2xl ml-13 mt-4 font-semibold font-serif">
+        Best Sellers-Shopping Time
+      </h1>
+      <HomeProducts />
     </div>
-  )
-}
+  );
+} //my comment
 
-export default HomeParent
+export default HomeParent;
